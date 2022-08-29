23cc0da61e4c07919aea734b9c7da1164e665c6a
Kurban <ext_ksagitov@rencredit.ru>
2022-08-29 15:09:52 +0300

    set up navigation and dependency versions

---
+++ app/src/main/java/com/kurban/bankapp/presentation/HostActivity.kt
@@ -0,0 +1,25 @@
+package com.kurban.bankapp.presentation
+
+import android.os.Bundle
+import com.kurban.core_compose.theme.BankAppTheme
+import androidx.activity.compose.setContent
+import androidx.compose.runtime.CompositionLocalProvider
+import com.bumble.appyx.core.integration.NodeHost
+import com.bumble.appyx.core.integrationpoint.LocalIntegrationPoint
+import com.bumble.appyx.core.integrationpoint.NodeActivity
+import com.kurban.bankapp.presentation.navigation.ContainerNode
+
+class HostActivity : NodeActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContent {
+            BankAppTheme {
+                NodeHost(integrationPoint = appyxIntegrationPoint) {
+                    ContainerNode(buildContext = it)
+                }
+            }
+        }
+    }
+}
